f06ff158cfc86a483d2aaf14068aef9f9f54dc84
Michael Schenk <mschenk@edgeg.io>
2016-03-19 12:05:44 -0500

    Enhance event.Save tests

--- event/event_test.go
+++ event/event_test.go
@@ -16,22 +16,22 @@ import (
 
 func TestSave(t *testing.T) {
 
-	// Create directories and source file
+	// Setup directories and source file
 	rootPath, err := ioutil.TempDir("", "gtm")
 	if err != nil {
-		t.Errorf("Unable to create tempory directory %s for TestSave, %s", rootPath, err)
+		t.Fatalf("Unable to create tempory directory %s, %s", rootPath, err)
 	}
 	gtmPath := path.Join(rootPath, ".gtm")
 	if err := os.MkdirAll(gtmPath, 0700); err != nil {
-		t.Errorf("Unable to create tempory directory %s for TestSave, %s", gtmPath, err)
+		t.Fatalf("Unable to create tempory directory %s, %s", gtmPath, err)
 	}
 	sourcePath := path.Join(rootPath, "src")
 	if err := os.MkdirAll(sourcePath, 0700); err != nil {
-		t.Errorf("Unable to create tempory directory %s for TestSave, %s", sourcePath, err)
+		t.Fatalf("Unable to create tempory directory %s, %s", sourcePath, err)
 	}
 	sourceFile := path.Join(sourcePath, "source.go")
 	if err := ioutil.WriteFile(sourceFile, []byte{}, 0600); err != nil {
-		t.Errorf("Unable to create tempory file %s for TestSave, %s", sourceFile, err)
+		t.Fatalf("Unable to create tempory file %s, %s", sourceFile, err)
 	}
 	defer os.Remove(sourceFile)
 	defer os.Remove(rootPath)
@@ -41,6 +41,11 @@ func TestSave(t *testing.T) {
 	env.Now = func() time.Time { return time.Unix(100, 0) }
 	defer func() { env.Now = saveNow }()
 
+	// Call save on an uninitialized Git Metric project
+	if err := Save(sourceFile); err != env.ErrNotInitialized {
+		t.Errorf("Save(%s), want error %s, got error %s", sourceFile, env.ErrNotInitialized, err)
+	}
+
 	// Replace env.Paths with a mock
 	savePaths := env.Paths
 	env.Paths = func(path ...string) (string, string, error) {
@@ -48,9 +53,14 @@ func TestSave(t *testing.T) {
 	}
 	defer func() { env.Paths = savePaths }()
 
-	// Call Save with source file
+	// Call Save with an invalid source file
+	if err := Save(path.Join(sourcePath, "doesnotexist.go")); err != env.ErrFileNotFound {
+		t.Errorf("Save(%s), want error %s, got %s", sourceFile, env.ErrFileNotFound, err)
+	}
+
+	// Call Save with a valid source file
 	if err := Save(sourceFile); err != nil {
-		t.Errorf("Save(%s) returns error %s", sourceFile, err)
+		t.Errorf("Save(%s), want error nil, got %s", sourceFile, err)
 	}
 
 	// Is there one event file?
@@ -59,27 +69,27 @@ func TestSave(t *testing.T) {
 		t.Errorf("Save(%s) returns error %s when reading .gtm directory", sourceFile, err)
 	}
 	if len(files) != 1 {
-		t.Errorf("Save(%s), want event file count 1 but got %d", sourceFile, len(files))
+		t.Errorf("Save(%s), want file count 1, got %d", sourceFile, len(files))
 	}
 
 	// Does the event file have the right prefix?
 	if !strings.HasPrefix(files[0].Name(), fmt.Sprintf("%d-", epoch.MinuteNow())) {
-		t.Errorf("Save(%s), want event file prefix %s but got %s", sourceFile, fmt.Sprintf("%d-", epoch.MinuteNow()), files[0].Name())
+		t.Errorf("Save(%s), want file prefix %s, got %s", sourceFile, fmt.Sprintf("%d-", epoch.MinuteNow()), files[0].Name())
 	}
 
 	// Read the event file
 	b, err := ioutil.ReadFile(path.Join(gtmPath, files[0].Name()))
 	eventContent := string(b)
 	if err != nil {
-		t.Errorf("Save(%s), unable to read event file %s, %s", sourceFile, files[0].Name(), err)
+		t.Fatalf("Save(%s), unable to read event file %s, %s", sourceFile, files[0].Name(), err)
 	}
 
 	// Does the event file have the right content?
 	relPath, err := filepath.Rel(rootPath, sourceFile)
 	if err != nil {
-		t.Errorf("Save(%s), unable to create relative path for rootPath %s and sourcePath %s, %s", sourceFile, rootPath, sourcePath, err)
+		t.Fatalf("Save(%s), error creating relative path for rootPath %s and sourcePath %s, %s", sourceFile, rootPath, sourcePath, err)
 	}
 	if fmt.Sprintf("%s,%s", rootPath, relPath) != strings.TrimSpace(string(eventContent)) {
-		t.Errorf("Save(%s), want file contents %s but got %s", sourceFile, fmt.Sprintf("%s,%s", rootPath, relPath), string(eventContent))
+		t.Errorf("Save(%s), want file contents %s, got %s", sourceFile, fmt.Sprintf("%s,%s", rootPath, relPath), string(eventContent))
 	}
 }
