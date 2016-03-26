657c781d64fa924d74b50119a05882465a5f2108
Michael Schenk <mschenk@edgeg.io>
2016-03-26 09:39:22 -0500

    Add more test cases for processing metrics

--- metric/metric_test.go
+++ metric/metric_test.go
@@ -14,46 +14,6 @@ import (
 	"edgeg.io/gtm/env"
 )
 
-var testdata string = `
-1458681900:map[vim.log:1] 
-1458682260:map[cmd/commit.go:1] 
-1458683340:map[event/event.go:1] 
-1458682740:map[vim.log:1] 
-1458676380:map[scm/git.go:1] 
-1458682440:map[event/event.go:2 metric/metric.go:1] 
-1458683460:map[vim.log:2] 
-1458682200:map[event/event.go:2 cmd/commit.go:2] 
-1458911580:map[metric/metric.go:1] 
-1458683400:map[event/event.go:4 event/event_test.go:2] 
-1458911460:map[metric/metric.go:2] 
-1458682320:map[cmd/commit.go:1] 
-1458682500:map[event/event.go:1] 
-1458682560:map[event/event.go:2] 
-1458681780:map[vim.log:1] 
-1458682080:map[event/event.go:1] 
-1458682620:map[vim.log:1] 
-1458683580:map[vim.log:1] 
-1458676500:map[scm/git.go:1] 
-1458676560:map[scm/git.go:1] 
-1458682020:map[cmd/record.go:1 env/env.go:1] 
-1458682140:map[cmd/record.go:2 event/event.go:2] 
-1458911520:map[metric/metric.go:1] 
-1458676440:map[scm/git.go:1] 
-1458681840:map[vim.log:1] 
-1458682680:map[vim.log:1] 
-1458911400:map[event/event_test.go:2] 
-1458683520:map[vim.log:1]]
-
-6f53bc90ba625b5afaac80b422b44f1f609d6367:{Updated:true GitFile:event/event.go Time:380} 
-fd3de0b7135021cc4c5ef23b8bea9ff98b704c47:{Updated:true GitFile:scm/git.go Time:240} 
-26c5bdda12d74ceb9cf191911a79454bccd80640:{Updated:true GitFile:metric/metric.go Time:200} 
-e65b42b6bf1eda6349451b063d46134dd7ab9921:{Updated:true GitFile:event/event_test.go Time:80} 
-f93cea510c5049ff60ef12c62825a53f7d6e7d48:{Updated:true GitFile:cmd/record.go Time:60} 
-1301df137d0acac0abf8cdc29bb74ef39ad2b042:{Updated:true GitFile:env/env.go Time:30} 
-2dbf769f7faf2f921b89f3ff9d81d7b5e02a17a5:{Updated:true GitFile:vim.log Time:540} 
-c2369545266e4a15c3db04a9f52b021364330bb7:{Updated:true GitFile:cmd/commit.go Time:150}]
-`
-
 func TestAllocateTime(t *testing.T) {
 	cases := []struct {
 		metric   map[string]metricFile
@@ -145,7 +105,7 @@ func TestProcess(t *testing.T) {
 
 	err = Process(false)
 	if err != nil {
-		t.Fatalf("Process(false), want error nil, got %s", err)
+		t.Fatalf("Process(false) - test full commit, want error nil, got %s", err)
 	}
 
 	cmd = exec.Command("git", "notes", "--ref", "gtm", "show")
@@ -154,7 +114,7 @@ func TestProcess(t *testing.T) {
 		t.Fatalf("Unable to run git notes, %s", string(b))
 	}
 
-	want := []string{"total: 300", "event.go: 280", "event_test.go: 20"}
+	want := []string{"total: 300", "event.go: 280 [m]", "event_test.go: 20 [m]"}
 	for _, s := range want {
 		if !strings.Contains(string(b), s) {
 			t.Errorf("Process(false) - test full commit, \nwant \n%s, \ngot \n%s", s, string(b))
@@ -162,7 +122,7 @@ func TestProcess(t *testing.T) {
 
 	}
 
-	// Test Process with committing only one of the two git tracked files that have been modified
+	// Test Process by committing a tracked file that has been modified and one untracked file that is not added/commited
 
 	// change back to saved current working directory and setup
 	os.Chdir(savedCurDir)
@@ -200,16 +160,74 @@ func TestProcess(t *testing.T) {
 		t.Fatalf("Unable to run git notes, %s", string(b))
 	}
 
-	want = []string{"total: 20", "event_test.go: 20"}
+	want = []string{"total: 20", "event_test.go: 20 [m]"}
 	for _, s := range want {
 		if !strings.Contains(string(b), s) {
-			t.Errorf("Process(false), \nwant \n%s, \ngot \n%s", s, string(b))
+			t.Errorf("Process(false) - test partial commit, \nwant \n%s, \ngot \n%s", s, string(b))
 		}
 
 	}
 	p := path.Join(gtmPath, "6f53bc90ba625b5afaac80b422b44f1f609d6367.metric")
 	if !env.FileExists(p) {
 		t.Errorf("Process(false) - test partial commit, want file %s exist, got file exists false", p)
+	}
+
+	// Test Process by committing a tracked file that has been modified and one tracked file that is unmodified
+
+	// change back to saved current working directory and setup
+	os.Chdir(savedCurDir)
+	rootPath, gtmPath, f3 := processSetup(t)
+	defer f3()
+
+	// chandge working directory and initialize git repo
+	os.Chdir(rootPath)
+	cmd = exec.Command("git", "init")
+	b, err = cmd.Output()
+	if err != nil {
+		t.Fatalf("Unable to initialize git repo, %s", string(b))
+	}
+
+	// commit source files to git repo
+	cmd = exec.Command("git", "add", "event/event_test.go")
+	b, err = cmd.Output()
+	if err != nil {
+		t.Fatalf("Unable to run git add, %s", string(b))
+	}
+	cmd = exec.Command("git", "commit", "-m", "Initial commit")
+	b, err = cmd.Output()
+	if err != nil {
+		t.Fatalf("Unable to run git commit, %s", string(b))
+	}
+
+	// commit source files to git repo
+	cmd = exec.Command("git", "add", "event/event.go")
+	b, err = cmd.Output()
+	if err != nil {
+		t.Fatalf("Unable to run git add, %s", string(b))
+	}
+	cmd = exec.Command("git", "commit", "-m", "Second commit")
+	b, err = cmd.Output()
+	if err != nil {
+		t.Fatalf("Unable to run git commit, %s", string(b))
+	}
+
+	err = Process(false)
+	if err != nil {
+		t.Fatalf("Process(false) - test commit with readonly, want error nil, got %s", err)
+	}
+
+	cmd = exec.Command("git", "notes", "--ref", "gtm", "show")
+	b, err = cmd.Output()
+	if err != nil {
+		t.Fatalf("Unable to run git notes, %s", string(b))
+	}
+
+	want = []string{"total: 300", "event_test.go: 20 [r]", "event/event.go: 280 [m]"}
+	for _, s := range want {
+		if !strings.Contains(string(b), s) {
+			t.Errorf("Process(false) - test commit with readonly, \nwant \n%s, \ngot \n%s", s, string(b))
+		}
+
 	}
 }
 
