c585bb940358fd6bc17885673787bcd46a521119
Michael Schenk <mschenk@edgeg.io>
2016-11-05 18:28:52 -0500

    Add copyright notice to packages

--- report/report.go
+++ report/report.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Michael Schenk. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package report
 
 import (
