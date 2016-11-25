4a260078124aa5e5933ebe8d1f754642b91d8885
Michael Schenk <mschenk@edgeg.io>
2016-11-25 17:30:05 -0600

    Add report tests

--- command/report_test.go
+++ command/report_test.go
@@ -31,6 +31,93 @@ func TestReportDefaultOptions(t *testing.T) {
 	}
 }
 
+func TestReportAll(t *testing.T) {
+	repo := util.NewTestRepo(t, false)
+	defer repo.Remove()
+	repo.Seed()
+
+	repoPath := repo.PathIn("")
+	defer os.Chdir(repoPath)
+	os.Chdir(repoPath)
+
+	(InitCmd{Ui: new(cli.MockUi)}).Run([]string{})
+
+	ui := new(cli.MockUi)
+	c := ReportCmd{Ui: ui}
+
+	args := []string{"-all"}
+	rc := c.Run(args)
+
+	if rc != 0 {
+		t.Errorf("gtm report(%+v), want 0 got %d, %s", args, rc, ui.ErrorWriter.String())
+	}
+}
+func TestReportTimelineHours(t *testing.T) {
+	repo := util.NewTestRepo(t, false)
+	defer repo.Remove()
+	repo.Seed()
+
+	repoPath := repo.PathIn("")
+	defer os.Chdir(repoPath)
+	os.Chdir(repoPath)
+
+	(InitCmd{Ui: new(cli.MockUi)}).Run([]string{})
+
+	ui := new(cli.MockUi)
+	c := ReportCmd{Ui: ui}
+
+	args := []string{"-format", "timeline-hours"}
+	rc := c.Run(args)
+
+	if rc != 0 {
+		t.Errorf("gtm report(%+v), want 0 got %d, %s", args, rc, ui.ErrorWriter.String())
+	}
+}
+
+func TestReportTimelineCommits(t *testing.T) {
+	repo := util.NewTestRepo(t, false)
+	defer repo.Remove()
+	repo.Seed()
+
+	repoPath := repo.PathIn("")
+	defer os.Chdir(repoPath)
+	os.Chdir(repoPath)
+
+	(InitCmd{Ui: new(cli.MockUi)}).Run([]string{})
+
+	ui := new(cli.MockUi)
+	c := ReportCmd{Ui: ui}
+
+	args := []string{"-format", "timeline-commits"}
+	rc := c.Run(args)
+
+	if rc != 0 {
+		t.Errorf("gtm report(%+v), want 0 got %d, %s", args, rc, ui.ErrorWriter.String())
+	}
+}
+
+func TestReportFiles(t *testing.T) {
+	repo := util.NewTestRepo(t, false)
+	defer repo.Remove()
+	repo.Seed()
+
+	repoPath := repo.PathIn("")
+	defer os.Chdir(repoPath)
+	os.Chdir(repoPath)
+
+	(InitCmd{Ui: new(cli.MockUi)}).Run([]string{})
+
+	ui := new(cli.MockUi)
+	c := ReportCmd{Ui: ui}
+
+	args := []string{"-format", "files"}
+	rc := c.Run(args)
+
+	if rc != 0 {
+		t.Errorf("gtm report(%+v), want 0 got %d, %s", args, rc, ui.ErrorWriter.String())
+	}
+}
+
 func TestReportInvalidOption(t *testing.T) {
 	ui := new(cli.MockUi)
 	c := ReportCmd{Ui: ui}
