75ed867e666d5708f6c4e1c467abaccf3312a5f4
Michael Schenk <mschenk@edgeg.io>
2016-11-25 17:18:32 -0600

    gtm clean tests

---
+++ command/clean_test.go
@@ -0,0 +1,47 @@
+package command
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/git-time-metric/gtm/util"
+	"github.com/mitchellh/cli"
+)
+
+func TestCleanYes(t *testing.T) {
+	repo := util.NewTestRepo(t, false)
+	defer repo.Remove()
+	repo.Seed()
+
+	repoPath := repo.PathIn("")
+	defer os.Chdir(repoPath)
+	os.Chdir(repoPath)
+
+	(InitCmd{Ui: new(cli.MockUi)}).Run([]string{})
+
+	ui := new(cli.MockUi)
+	c := CleanCmd{Ui: ui}
+
+	args := []string{"-yes"}
+	rc := c.Run(args)
+
+	if rc != 0 {
+		t.Errorf("gtm clean(%+v), want 0 got %d, %s", args, rc, ui.ErrorWriter.String())
+	}
+}
+
+func TestCleanInvalidOption(t *testing.T) {
+	ui := new(cli.MockUi)
+	c := CleanCmd{Ui: ui}
+
+	args := []string{"-invalid"}
+	rc := c.Run(args)
+
+	if rc != 1 {
+		t.Errorf("gtm clean(%+v), want 0 got %d, %s", args, rc, ui.ErrorWriter)
+	}
+	if !strings.Contains(ui.OutputWriter.String(), "Usage:") {
+		t.Errorf("gtm clean(%+v), want 'Usage:'  got %d, %s", args, rc, ui.OutputWriter.String())
+	}
+}
