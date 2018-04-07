ba9e325c0be576af9eacd488faae8d2506a3ff52
Michael Schenk <mschenk@theschenks.name>
2018-04-07 11:17:19 -0500

    remove cmake hacks

--- script/git2go-init.sh
+++ script/git2go-init.sh
@@ -9,4 +9,4 @@ git checkout -qf $COMMIT_HASH
 git submodule update --init
 # sed -i -- 's/ZLIB_FOUND/FALSE/g' $PROJPATH/vendor/libgit2/CMakeLists.txt
 # sed -i -- 's/OPENSSL_FOUND/FALSE/g' $PROJPATH/vendor/libgit2/CMakeLists.txt
-sed -i -- 's/USE_SSH.*"Link with libssh to enable SSH support".*ON/USE_SSH  "Link with libssh to enable SSH support"  OFF/g' $PROJPATH/vendor/libgit2/CMakeLists.txt
+# sed -i -- 's/USE_SSH.*"Link with libssh to enable SSH support".*ON/USE_SSH  "Link with libssh to enable SSH support"  OFF/g' $PROJPATH/vendor/libgit2/CMakeLists.txt
