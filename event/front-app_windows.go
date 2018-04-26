7d78c01c869046cc9475c6c2543222da1f7bc0b4
Michael Schenk <mschenk@theschenks.name>
2018-04-26 17:04:02 -0500

    remove nil check for window handle

--- event/front-app_windows.go
+++ event/front-app_windows.go
@@ -1,20 +1,13 @@
 package event
 
 import (
-	"errors"
-
 	"github.com/git-time-metric/w32"
 	ps "github.com/mitchellh/go-ps"
 )
 
 func getFrontApp() (string, error) {
 	hwnd := w32.GetWindowForeground()
-	if hwnd == nil {
-		return "", errors.New("Unable to get window in the foreground")
-	}
-
 	_, pid := w32.GetWindowThreadProcessId(hwnd)
-
 	p, err := ps.FindProcess(pid)
 	if err != nil {
 		return "", err
